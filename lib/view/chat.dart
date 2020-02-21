3d01699e2b9e6443bbfe0b417a30ba871269e159
SMMEGAtive <31795316+SMMEGAtive@users.noreply.github.com>
2020-02-21 16:56:45 +0700

    initial add

---
+++ lib/view/chat.dart
@@ -0,0 +1,20 @@
+import 'package:competition_arena/components/app_bar_custom.dart';
+import 'package:flutter/material.dart';
+
+class Chat extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBarCustom(
+        hasBackButton: true,
+        title: 'Nama Penggunanya',
+      ),
+    );
+  }
+}
