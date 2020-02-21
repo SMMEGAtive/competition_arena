aa782c88b341d2249479b3e6a936063127005f74
SMMEGAtive <31795316+SMMEGAtive@users.noreply.github.com>
2020-02-21 15:16:15 +0700

    added more stuff like cover images and descriptions

--- lib/view/competition.dart
+++ lib/view/competition.dart
@@ -1,8 +1,29 @@
+import 'package:competition_arena/components/app_bar_custom.dart';
 import 'package:flutter/material.dart';
 
 class Competition extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBarCustom(
+        hasBackButton: true,
+        title: 'Lomba 1',
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            child: Image.network('https://rhythmega.s-ul.eu/KGwwejMs',
+                fit: BoxFit.cover),
+          ),
+          Flexible(
+            child: Container(
+              child: Text('Description'),
+            ),
+          ),
+          Text('')
+        ],
+      ),
+    );
   }
 }
