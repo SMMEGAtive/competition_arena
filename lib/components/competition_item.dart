88a5c0d6432e1b45474753ab4a715cb0353085c4
SMMEGAtive <31795316+SMMEGAtive@users.noreply.github.com>
2020-02-21 09:26:29 +0700

    initial creation

---
+++ lib/components/competition_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CompetitionItem extends StatelessWidget {
+  final String title;
+  final String date;
+  final String imageURL;
+  final String status;
+  final String description;
+
+  CompetitionItem({
+    Key key,
+    this.title,
+    this.date,
+    this.imageURL,
+    this.status,
+    this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget item = Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+        ),
+        borderRadius: BorderRadius.horizontal(
+          left: Radius.circular(10),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            width: 120,
+            height: 120,
+            child: Image.network(
+              imageURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(title),
+              Text('Tanggal ' + status + ':'),
+              Text(date),
+              Text(description)
+            ],
+          )
+        ],
+      ),
+    );
+
+    return item;
+  }
+}
---
+++ lib/components/competition_list.dart
@@ -0,0 +1,46 @@
+import 'package:competition_arena/components/competition_item.dart';
+import 'package:flutter/material.dart';
+
+class CompetitionList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CompetitionListState();
+}
+
+class _CompetitionListState extends State<CompetitionList> {
+  @override
+  Widget build(BuildContext context) {
+    var list = ListView.builder(
+      //itemCount harus dibikin dinamis nanti
+      itemCount: 3,
+      scrollDirection: Axis.vertical,
+
+      //Buat build list
+      itemBuilder: (context, index) {
+        return CompetitionItem(
+          title: 'Lomba 1',
+          date: 'Tanggalnya',
+          imageURL: 'https://rhythmega.s-ul.eu/v9SyAUOe',
+          description: 'Deskripsi lombanya',
+          status: 'Pendaftaran',
+        );
+      },
+    );
+
+    var fullList = Container(
+      height: 300,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              child: list,
+            ),
+          )
+        ],
+      ),
+    );
+
+    return fullList;
+  }
+}
