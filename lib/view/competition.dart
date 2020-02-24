b26769a3db4dc7986c174e83144b20f8f1d4998a
SMMEGAtive <31795316+SMMEGAtive@users.noreply.github.com>
2020-02-24 13:35:20 +0700

    fixed visualization

--- lib/view/competition.dart
+++ lib/view/competition.dart
@@ -1,4 +1,5 @@
 import 'package:competition_arena/components/app_bar_custom.dart';
+import 'package:competition_arena/view/chat.dart';
 import 'package:flutter/material.dart';
 
 class Competition extends StatelessWidget {
@@ -8,26 +9,85 @@ class Competition extends StatelessWidget {
       appBar: AppBarCustom(
         hasBackButton: true,
         title: 'Lomba 1',
+        actions: <Widget>[
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Chat(),
+                ),
+              );
+            },
+            child: Text('Chat'),
+          )
+        ],
       ),
-      body: Column(
+      body: Stack(
         children: <Widget>[
-          Container(
-            height: 300,
-            child: Image.network('https://rhythmega.s-ul.eu/KGwwejMs',
-                fit: BoxFit.cover),
-          ),
-          Flexible(
-            child: Container(
-              child: Text('Description'),
-            ),
-          ),
-          Stack(
+          Column(
             children: <Widget>[
-              Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: Container(child: Text('Bottom Bar')),
+              Container(
+                height: 200,
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(
+                      'https://rhythmega.s-ul.eu/KGwwejMs',
+                      fit: BoxFit.cover,
+                      color: Color.fromRGBO(255, 255, 255, 0.15),
+                      colorBlendMode: BlendMode.modulate,
+                    ),
+                    Positioned(
+                      child: Text(
+                        'Lomba 1',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w900),
+                      ),
+                      bottom: 60,
+                      left: 15,
+                    ),
+                    Positioned(
+                      child: Text(
+                        'Penyelenggara',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      bottom: 40,
+                      left: 15,
+                    ),
+                    Positioned(
+                      child: Text(
+                        'Tanggal',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      bottom: 20,
+                      left: 15,
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(border: Border.all(width: 1)),
+                  child: Text(
+                      'Description DescriptionDescription Description Description Description Description Description Description Description Description Description Description Description Description'),
+                ),
               ),
             ],
+          ),
+          Positioned(
+            child: Align(
+              alignment: FractionalOffset.bottomCenter,
+              child: Container(
+                width: 300,
+                padding: EdgeInsets.all(10),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Button Bar'),
+                ),
+              ),
+            ),
           ),
         ],
       ),
