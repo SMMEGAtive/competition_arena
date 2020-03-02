7d717779708e16e62a062a16514863d57b1b9e33
SMMEGAtive <31795316+SMMEGAtive@users.noreply.github.com>
2020-03-02 12:16:11 +0700

    updated main routing to primary page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:competition_arena/view/home.dart';
+import 'package:competition_arena/view/primary_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: PrimaryPage(),
     );
   }
 }
